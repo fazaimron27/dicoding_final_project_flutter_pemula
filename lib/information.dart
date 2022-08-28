c8eaa7e1182d647aeed6daf089febeeb17c8984f
fazaimron27 <cincaumyself@gmail.com>
2022-08-28 23:38:50 +0700

    feat: modify information page

--- lib/information.dart
+++ lib/information.dart
@@ -1,16 +1,101 @@
 import 'package:flutter/material.dart';
+import 'package:dicoding_final_project_flutter_pemula/models/information_model.dart';
 
 class Information extends StatelessWidget {
   const Information({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Icon(
-          Icons.info,
-          size: 150,
-        ),
+    InformationModel information = InformationModel();
+
+    return Scaffold(
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Information',
+                style: TextStyle(
+                  fontSize: 36,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Name : ${information.name}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'SIB-ID : ${information.id}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Email : ${information.email}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Instagram : ${information.instagram}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Facebook : ${information.facebook}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Linkedin : ${information.linkedin}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Github : ${information.github}',
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
---
+++ lib/models/information_model.dart
@@ -0,0 +1,9 @@
+class InformationModel {
+  String name = 'Faza Iman Imron';
+  String id = 'P183X0331';
+  String email = 'P183X0331@dicoding.org';
+  String instagram = '@faza_imron27';
+  String facebook = 'fazaimanimron';
+  String linkedin = 'fazaimron27';
+  String github = 'fazaimron27';
+}
