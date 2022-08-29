a68c4ac9c094ccbfd7d1810cfe14580cbbb4ce79
fazaimron27 <cincaumyself@gmail.com>
2022-08-29 11:05:58 +0700

    perf: improvements movies page for web development

--- lib/detail_movie.dart
+++ lib/detail_movie.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:dicoding_final_project_flutter_pemula/models/movies_model.dart';
 
@@ -9,15 +11,207 @@ class DetailMovie extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          return DetailMovieWeb(movie: movie);
+        } else {
+          return DetailMovieMobile(movie: movie);
+        }
+      },
+    );
+  }
+}
+
+class DetailMovieMobile extends StatelessWidget {
+  final MoviesModel movie;
+
+  const DetailMovieMobile({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
               children: <Widget>[
-                Stack(
-                  children: <Widget>[
-                    Image.asset(movie.poster),
+                Image.asset(movie.poster),
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.blueGrey,
+                          child: IconButton(
+                            icon: const Icon(
+                              Icons.close,
+                              color: Colors.white,
+                            ),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 24),
+              child: Text(
+                movie.title,
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10),
+              padding: const EdgeInsets.all(15),
+              child: Text(
+                movie.plot,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto Italic',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Genre: ${movie.genre}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Duration: ${movie.duration}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Relese Date: ${movie.releaseDate}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Country: ${movie.country}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Director: ${movie.director}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Writer: ${movie.writer}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Platform: ${movie.platform}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 10, left: 15),
+              child: Text(
+                "Cast: ${movie.cast}",
+                textAlign: TextAlign.start,
+                style: const TextStyle(
+                  fontSize: 16.0,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DetailMovieWeb extends StatefulWidget {
+  final MoviesModel movie;
+
+  const DetailMovieWeb({Key? key, required this.movie}) : super(key: key);
+
+  @override
+  _DetailMovieWebPageState createState() => _DetailMovieWebPageState();
+}
+
+class _DetailMovieWebPageState extends State<DetailMovieWeb> {
+  final _scrollController = ScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: kIsWeb ? null : AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 64,
+        ),
+        child: Center(
+          child: SizedBox(
+            width: screenWidth <= 1200 ? 800 : 1200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const SizedBox(height: 32),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     SafeArea(
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
@@ -40,124 +234,158 @@ class DetailMovie extends StatelessWidget {
                         ),
                       ),
                     ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(widget.movie.poster),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 32),
+                    Expanded(
+                      child: Card(
+                        child: Container(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: <Widget>[
+                              Text(
+                                widget.movie.title,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 30.0,
+                                  fontFamily: 'Roboto',
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(top: 10),
+                                padding: const EdgeInsets.all(15),
+                                child: Text(
+                                  widget.movie.plot,
+                                  textAlign: TextAlign.justify,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto Italic',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Genre: ${widget.movie.genre}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Duration: ${widget.movie.duration}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Relese Date: ${widget.movie.releaseDate}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Country: ${widget.movie.country}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Director: ${widget.movie.director}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Writer: ${widget.movie.writer}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Platform: ${widget.movie.platform}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.only(top: 10, left: 15),
+                                child: Text(
+                                  "Cast: ${widget.movie.cast}",
+                                  textAlign: TextAlign.start,
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontFamily: 'Roboto',
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
                   ],
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 24),
-                  child: Text(
-                    movie.title,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 20.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10),
-                  padding: const EdgeInsets.all(15),
-                  child: Text(
-                    movie.plot,
-                    textAlign: TextAlign.justify,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto Italic',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Genre: ${movie.genre}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Duration: ${movie.duration}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Relese Date: ${movie.releaseDate}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Country: ${movie.country}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Director: ${movie.director}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Writer: ${movie.writer}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Platform: ${movie.platform}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 10, left: 15),
-                  child: Text(
-                    "Cast: ${movie.cast}",
-                    textAlign: TextAlign.start,
-                    style: const TextStyle(
-                      fontSize: 16.0,
-                      fontFamily: 'Roboto',
-                    ),
-                  ),
                 ),
               ],
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
   }
 }
--- lib/movies.dart
+++ lib/movies.dart
@@ -7,6 +7,25 @@ class Movies extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth <= 600) {
+          return const MoviesList();
+        } else if (constraints.maxWidth <= 1200) {
+          return const MoviesGrid(gridCount: 4);
+        } else {
+          return const MoviesGrid(gridCount: 6);
+        }
+      },
+    );
+  }
+}
+
+class MoviesList extends StatelessWidget {
+  const MoviesList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         final MoviesModel movies = moviesList[index];
@@ -27,13 +46,13 @@ class Movies extends StatelessWidget {
                   image: AssetImage(movies.poster),
                   fit: BoxFit.cover,
                 ),
-                borderRadius: BorderRadius.circular(4),
+                borderRadius: BorderRadius.circular(12),
               ),
               child: Container(
                 alignment: Alignment.bottomRight,
                 padding: const EdgeInsets.all(12),
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(4),
+                  borderRadius: BorderRadius.circular(12),
                   color: Colors.black.withOpacity(0.5),
                 ),
                 child: Text(
@@ -51,6 +70,65 @@ class Movies extends StatelessWidget {
         );
       },
       itemCount: moviesList.length,
+    );
+  }
+}
+
+class MoviesGrid extends StatelessWidget {
+  final int gridCount;
+
+  const MoviesGrid({Key? key, required this.gridCount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: GridView.count(
+        crossAxisCount: gridCount,
+        crossAxisSpacing: 16,
+        mainAxisSpacing: 16,
+        children: moviesList.map((movies) {
+          return InkWell(
+            onTap: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return DetailMovie(movie: movies);
+              }));
+            },
+            child: Card(
+              elevation: 6,
+              margin: const EdgeInsets.all(12),
+              child: Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(movies.poster),
+                    fit: BoxFit.cover,
+                  ),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Container(
+                  alignment: Alignment.bottomRight,
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  child: Text(
+                    movies.title,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto Italic',
+                      color: Colors.white70,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
